7bdb5e95ff2a0574b53281afb8cae6ddb0830132
Janusz Grabis <janusz@rsi.pl>
2014-04-16 06:51:21 +0200

    Extending metadata to support RightScale

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,11 @@ description      'Installs/Configures wildfly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+recipe "wildfly::default", ""
+recipe "wildfly::install", ""
+recipe "wildfly::mysql_connector", ""
+recipe :wildfly::mysql_datasources", ""
+
 supports 'centos'
 
 depends 'apt'
